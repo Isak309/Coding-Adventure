4377ff89ae2716e3273a90de6a3d8db41b02e774
isak309 <isak.angerstig@gmail.com>
2020-02-05 21:17:35 -0600

    Character creation + GUI buttons
    Created character and implemented Flip() function and walk physics
    
    Added buttons to GUI that change scenes

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //speed of character
+    public float topSpeed = 10f;
+    //Which way the sprite is facing
+    bool facingRight = true;
+
+    private void FixedUpdate()
+    {
+        //get move direction
+        float move = Input.GetAxis("Horizontal");
+
+        //add velocity to the rigidbody in the move direction * our speed
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        //if we're facing negative direcetion and not facing right, flip
+        if(move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        //saying we are facing opposite direction
+        facingRight = !facingRight;
+
+        //get the local scale
+        Vector3 theScale = transform.localScale;
+
+        //flip on x axis
+        theScale.x *= -1;
+
+        //apply that to the local scale
+        transform.localScale = theScale;
+    }
+}
