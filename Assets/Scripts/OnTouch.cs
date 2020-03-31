50847f7b300db74c5efaa2e371d544a3eaed91db
isak309 <isak.angerstig@gmail.com>
2020-03-30 20:04:21 -0500

    Revert "asdf"
    This reverts commit a6ee2cb4160ad8dc88334c1d4648797218521472.

--- Assets/Scripts/OnTouch.cs
+++ Assets/Scripts/OnTouch.cs
@@ -26,13 +26,12 @@ public class OnTouch : MonoBehaviour
         //Check incorrect
         foreach (Touch touch in Input.touches)
         {
-            if (touch.position.x < Screen.width / 3 && touch.position.y < Screen.height / 2.2)
+            if (touch.position.x < Screen.width / 2)
             {
                 Debug.Log("Touch");
             }
             else
             {
-                Debug.Log("Other Touch");
                 Vector2 test = Camera.main.ScreenToWorldPoint(touch.position);
                 RaycastHit2D hit = Physics2D.Raycast(test, (touch.position));
                 if (hit.collider && hit.collider.tag == "CorrectText")
