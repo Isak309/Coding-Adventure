353aa34cf6168d5bd3abf47dc6e359bff3de4baf
SarahMeagan14 <60491657+SarahMeagan14@users.noreply.github.com>
2020-04-20 22:35:42 -0500

    Tab Key and OnFocus Functionalities
    Updated so that the first field on the Login and Sign-Up forms are focused upon loading. The tab key can also be used to switch between fields.

--- Assets/Login.cs
+++ Assets/Login.cs
@@ -2,25 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Login : MonoBehaviour
 {
     public InputField UsernameInput;
     public InputField PasswordInput;
     public Button LoginButton;
+    EventSystem system;
     // Start is called before the first frame update
     void Start()
     {
+        UsernameInput.Select();
+        UsernameInput.ActivateInputField();
         LoginButton.onClick.AddListener(() =>
         {
             StartCoroutine(Main.Instance.Web.Login(UsernameInput.text, PasswordInput.text));
         });
+
+        system = EventSystem.current;
        
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+
+            if (next != null)
+            {
+
+                InputField inputfield = next.GetComponent<InputField>();
+                if (inputfield != null) inputfield.OnPointerClick(new PointerEventData(system));  //if it's an input field, also set the text caret
+
+                system.SetSelectedGameObject(next.gameObject, new BaseEventData(system));
+            }
+            //else Debug.Log("next nagivation element not found");
+
+        }
     }
 }
--- Assets/RegisterUser.cs
+++ Assets/RegisterUser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class RegisterUser : MonoBehaviour
 {
@@ -9,19 +10,37 @@ public class RegisterUser : MonoBehaviour
     public InputField UsernameInput;
     public InputField PasswordInput;
     public Button RegisterButton;
+    EventSystem system;
     // Start is called before the first frame update
     void Start()
     {
+        EmailInput.Select();
+        EmailInput.ActivateInputField();
         RegisterButton.onClick.AddListener(() =>
         {
             StartCoroutine(Main.Instance.Web.RegisterUser(EmailInput.text, UsernameInput.text, PasswordInput.text));
         });
 
+        system = EventSystem.current;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+
+            if (next != null)
+            {
+
+                InputField inputfield = next.GetComponent<InputField>();
+                if (inputfield != null) inputfield.OnPointerClick(new PointerEventData(system));  //if it's an input field, also set the text caret
+
+                system.SetSelectedGameObject(next.gameObject, new BaseEventData(system));
+            }
+            //else Debug.Log("next nagivation element not found");
+
+        }
     }
 }
